b0f515fb7d7c690a39d092a96df4b6d8008ee720
kmno4hno3 <kmno4hno3@ezweb.ne.jp>
2020-08-07 17:38:51 +0900

    add_tasklist & newtask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,36 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def edit
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    logger.debug("~~~~~~~~~")
+    logger.debug(task_params)
+    logger.debug("~~~~~~~~~")
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url
   end
 
   def show
   end
+
+  def destroy
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+
 end
